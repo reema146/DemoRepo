95adfc2ec2c722475bcdbea78d0818f6a41a33fd
reema146 <76464921+reema146@users.noreply.github.com>
2020-12-29 10:58:15 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
-public class Demo {
- public static void main(String[] args){
+class Demo
+{
+public static void main(String[] args){
 System.out.println(" Hello world 1");
 System.out.println("Hello All");
 System.out.println(" Hello world 2");
